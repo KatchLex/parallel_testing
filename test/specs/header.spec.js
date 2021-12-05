1f9156b53c2641745ff6603f36719bb40dbdda37
Alexander Kachalov <Alexander_Kachalov@epam.com>
2021-12-05 18:54:06 +0300

    webdriver-manager update

--- test/specs/header.spec.js
+++ test/specs/header.spec.js
@@ -18,16 +18,16 @@ describe("Home page header", function () {
     });
 
     // This an example of a bad implementation, don't to it this way EVER!
-    xit("should have 6 menu items (Example of how NOT to do it)", async function () {
-        await browser.get("https://www.epam.com");
-        const arrayOfelementsTexts = await element.all(by.css("div.header .top-navigation__item")).getText();
-        const elementToClickIndex = arrayOfelementsTexts.indexOf("OUR WORK");
-        const elementToClick = element.all(by.css("div.header .top-navigation__item")).get(elementToClickIndex);
-        await elementToClick.click();
-        const firstNavigationButton = element.all(by.css("div.header .top-navigation__item")).get(0);
-        await browser.wait(EC.elementToBeClickable(firstNavigationButton), 10000);
-        const countOfNavigationButtons = await element.all(by.css("div.header .top-navigation__item")).count();
-        expect(countOfNavigationButtons).to.be.equal(6);
-    });
+    // xit("should have 6 menu items (Example of how NOT to do it)", async function () {
+    //     await browser.get("https://www.epam.com");
+    //     const arrayOfelementsTexts = await element.all(by.css("div.header .top-navigation__item")).getText();
+    //     const elementToClickIndex = arrayOfelementsTexts.indexOf("OUR WORK");
+    //     const elementToClick = element.all(by.css("div.header .top-navigation__item")).get(elementToClickIndex);
+    //     await elementToClick.click();
+    //     const firstNavigationButton = element.all(by.css("div.header .top-navigation__item")).get(0);
+    //     await browser.wait(EC.elementToBeClickable(firstNavigationButton), 10000);
+    //     const countOfNavigationButtons = await element.all(by.css("div.header .top-navigation__item")).count();
+    //     expect(countOfNavigationButtons).to.be.equal(6);
+    // });
 
 });
