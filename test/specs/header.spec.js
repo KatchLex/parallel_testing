db02e83bac00468cbc94696f17005e44abeeb826
Alexander Kachalov <Alexander_Kachalov@epam.com>
2021-12-09 00:06:17 +0300

    non-angular app test

--- test/specs/header.spec.js
+++ test/specs/header.spec.js
@@ -1,20 +1,23 @@
 const PageFactory = require("../utils/page_objects/pageFactory");
-const until = protractor.ExpectedConditions;
+const EC = protractor.ExpectedConditions;
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000;
+const {Builder, By, Key} = require('selenium-webdriver');
+//const chrome = require('selenium-webdriver/chrome');
+
 describe("Protractor", function () {
 
     beforeEach(function() {
-            browser.ignoreSynchronization = true;
+            //browser.ignoreSynchronization = true;
             return browser.manage().window().maximize();
     });
 
     it("should open Region Selector Page with Continue button", async function () {
         await PageFactory.getPage("Home").open();
         await PageFactory.getPage("Home").Header.clickSelectRegionDropdown();
-        browser.wait(until.visibilityOf(element(by.linkText("Other country or region"))), 6000);
+        browser.wait(EC.visibilityOf(element(by.linkText("Other country or region"))), 6000);
         await PageFactory.getPage("Home").Header.clickOtherCountryOption();
         await PageFactory.getPage("Home").Header.clickContinueButton();
-        const currentURL = browser.wait(until.urlIs("https://www.westerndigital.com/en-us/region-selector"), 6000);
+        const currentURL = browser.wait(EC.urlIs("https://www.westerndigital.com/en-us/region-selector"), 6000);
         expect(currentURL).toBe(true, "Current URL Not Found");
         });
 
@@ -23,7 +26,7 @@ describe("Protractor", function () {
         await PageFactory.getPage("Select Your Region").clickSelectUsEnglish();
         await PageFactory.getPage("Home").clickExpandSearchField();
         await element(by.className('search expandright searchBTN')).sendKeys("G-drive", protractor.Key.ENTER);
-        browser.wait(until.visibilityOf(element(by.xpath("//input[@value='Products']/.."))), 6000);
+        browser.wait(EC.visibilityOf(element(by.xpath("//input[@value='Products']/.."))), 6000);
         const arrayOfHeaderTexts = await element.all(by.xpath("//h3/*[1]")).getText();
         expect(arrayOfHeaderTexts.length).toEqual(10);
         const result = arrayOfHeaderTexts.every(element => element.includes("G-"));
@@ -34,8 +37,17 @@ describe("Protractor", function () {
         await PageFactory.getPage("Blog").open();
         await PageFactory.getPage("Blog").navigationButtons.clickElementByText("People");
         const mainNavigationButton = PageFactory.getPage("Blog").navigationButtons.collection.get(0);
-        browser.wait(until.elementToBeClickable(mainNavigationButton), 6000);
+        browser.wait(EC.elementToBeClickable(mainNavigationButton), 6000);
         const countOfNavigationButtons = await PageFactory.getPage("Blog").navigationButtons.getCount();
         expect(countOfNavigationButtons).toEqual(4);
+    });
+
+    it("Handler", async function () {
+        // chromeOptions = new chrome.Options();
+        // chromeOptions.addArguments('--start-maximized');
+        let driver = await new Builder().forBrowser('chrome').build();
+        await driver.get('https://www.handler.by');
+        await driver.findElement(By.className('top-btn inline-search-show twosmallfont')).click();
+        await driver.waitForVisibleElement(By.id('title-search-input'), 2000).sendKeys('системы', Key.ENTER);
     });
 });
